e313b84b770fc2bf7a227adf7fa47946dd078a63
Socrates <32815437+TheKingpin221@users.noreply.github.com>
2022-09-04 12:11:37 +0200

    Commit - node and models

---
+++ Models/model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    shopID: {
+        required: true,
+        type: String
+    },
+    clientName:{
+        required:true,
+        type:String
+    },
+    arrivalTime:{
+        required: true,
+        type:String
+    },
+    contact:{
+        required:true,
+        type:String
+    },
+    dateOfArrival:{
+        type:String,
+        required:true,
+    }
+
+})
+
+module.exports = mongoose.model('Data', dataSchema,'Appointments')
